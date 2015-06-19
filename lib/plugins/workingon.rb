46546644eaadab89ee6b2949b99d7d2ead6371d9
Ryan Priebe <rpriebe@me.com>
2015-06-19 13:54:46 -0600

    add new digest logging method and workingon digest log file

--- lib/plugins/workingon.rb
+++ lib/plugins/workingon.rb
@@ -1,7 +1,21 @@
 module Plugin
   class WorkingOn
     def send(message)
-      ::Net::HTTP.post_form(URI(sprintf(api_endpoint, api_key)), :task => CGI.escapeHTML(message))
+      log = File.new('./wo-digest.log')
+
+      open(log, 'a') do |file|
+        file.write("#{message}\n")
+      end
+
+      # enough messages have been stored, send digest
+      if Plugin::num_lines(log) > 3
+        digest = open(log, 'r').read
+
+        Net::HTTP.post_form(URI(sprintf(api_endpoint, api_key)), :task => CGI.escapeHTML(digest.split("\n").uniq.join(', ')))
+        
+        # reset digest after sending
+        File.truncate(log, 0)
+      end
     end
 
     def api_endpoint
