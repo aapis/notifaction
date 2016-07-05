aff0368faa6f41cbc4d09072f969237bc286aa89
Ryan Priebe <rpriebe@me.com>
2016-07-05 17:04:29 -0600

    added tests for all methods

--- test/notifaction_test.rb
+++ test/notifaction_test.rb
@@ -1,11 +1,37 @@
 require 'test_helper'
 
 class NotifactionTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Notifaction::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_success
+    assert Notify.success("A test message", { print: false })
   end
+
+  def test_error
+    refute Notify.error("A test message", { print: false, auto_quit: false })
+  end
+
+  def test_warning
+    assert Notify.warning("A test message", { print: false })
+  end
+
+  def test_spacer
+    assert Notify.spacer({ print: false })
+  end
+
+  def test_info
+    assert Notify.info("A test message", { print: false })
+  end
+
+  def test_note
+    assert Notify.note("A test message", { print: false })
+  end
+
+  def test_spit
+    assert Notify.spit("A test message", { print: false })
+  end
+
 end
