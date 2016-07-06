603d1d82c4361c2a51cdd3abd1fbadcff9cf6a75
Ryan Priebe <rpriebe@me.com>
2016-07-06 11:14:52 -0600

    renamed plugin namespace to hooks to reflect the new functionality

--- lib/notifaction.rb
+++ lib/notifaction.rb
@@ -1,14 +1,12 @@
 require 'time'
 require 'rbconfig'
-require 'net/http'
-require 'cgi'
 
 require 'notifaction/type'
 require 'notifaction/types/linux'
 require 'notifaction/types/osx'
 require 'notifaction/types/terminal'
 require 'notifaction/notify'
-require 'notifaction/plugin'
+require 'notifaction/hooks'
 require "notifaction/version"
 require 'notifaction/utils'
 
