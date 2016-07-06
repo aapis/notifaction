319fd37faae373b5a50f8b870b5221c84024dd12
Ryan Priebe <rpriebe@me.com>
2016-07-06 12:15:31 -0600

    hound fixes

--- lib/notifaction/style.rb
+++ lib/notifaction/style.rb
@@ -2,26 +2,26 @@ module Notifaction
   module Style
 
     @map = {
-      :colour => {
-        :red => 31,
-        :green => 32,
-        :yellow => 33,
-        :blue => 34,
-        :magenta => 35,
-        :cyan => 36,
-        :white => 37,
-        :null => 0
+      colour: {
+        red: 31,
+        green: 32,
+        yellow: 33,
+        blue: 34,
+        magenta: 35,
+        cyan: 36,
+        white: 37,
+        null: 0
       },
-      :style => {
-        :reset => 0,
-        :bold => 1,
-        :underline => 4,
-        :normal => ""
+      style: {
+        reset: 0,
+        bold: 1,
+        underline: 4,
+        normal: ""
       }
     }
 
     def self.format(message, colour = nil, style = nil)
-      c = @map[:colour][colour.to_sym] if !colour.nil?
+      c = @map[:colour][colour.to_sym] unless colour.nil?
 
       if style.nil?
         t = 0
